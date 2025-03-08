1579633060ec9548cc7205c33c0fb37f5312d5b1
ssingh <sp@Sants-MacBook-Pro.local>
2025-03-08 13:50:29 -0800

    iterm colors update + nvim explorer right

--- .config/nvim/lua/plugins/lazyoverride.lua
+++ .config/nvim/lua/plugins/lazyoverride.lua
@@ -13,4 +13,16 @@ return {
       },
     },
   },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            layout = { layout = { position = "right" } },
+          },
+        },
+      },
+    },
+  },
 }
